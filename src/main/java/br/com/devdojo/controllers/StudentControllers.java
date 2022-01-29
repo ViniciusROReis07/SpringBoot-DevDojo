299e5ba14b175a67dea8f571652093e1af4cb188
Vinicius Rhuan Oliveira Reis <60737732+ViniciusROReis07@users.noreply.github.com>
2022-01-28 23:09:42 -0300

    feat: Requisições post e get

--- src/main/java/br/com/devdojo/controllers/StudentControllers.java
+++ src/main/java/br/com/devdojo/controllers/StudentControllers.java
@@ -2,6 +2,7 @@ package br.com.devdojo.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,14 @@ public class StudentControllers {
 
     private List<Student> students = new ArrayList<>();
 
+    @RequestMapping("/list")
     public List<Student> listAll() {
         return this.students;
+    }
+
+    @RequestMapping("/cadastrar")
+    public Student cadastrar(@RequestBody Student student) {
+        this.students.add(student);
+        return student;
     }
 }
