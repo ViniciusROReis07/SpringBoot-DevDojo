89834ed354dafec6f9f3426eef2b60adec14413c
Vinicius Rhuan Oliveira Reis <60737732+ViniciusROReis07@users.noreply.github.com>
2022-01-28 23:45:48 -0300

    feat: Componente de data

--- src/main/java/br/com/devdojo/controllers/StudentControllers.java
+++ src/main/java/br/com/devdojo/controllers/StudentControllers.java
@@ -2,21 +2,28 @@ package br.com.devdojo.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.devdojo.model.Student;
+import br.com.devdojo.util.DateUtil;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 @RestController
 @RequestMapping("student")
 public class StudentControllers {
 
+    @Autowired
+    private DateUtil dateUtil;
     private List<Student> students = new ArrayList<>();
 
     @RequestMapping("/list")
     public List<Student> listAll() {
+        System.out.println("Date now "+dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return this.students;
     }
 
