ffd6eddfac89c9bb64e5c0d9d9b8c486064d88d1
Vinicius Rhuan Oliveira Reis <60737732+ViniciusROReis07@users.noreply.github.com>
2022-02-04 23:50:59 -0300

    feat: Put student

--- src/main/java/br/com/devdojo/controllers/StudentControllers.java
+++ src/main/java/br/com/devdojo/controllers/StudentControllers.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,6 +63,13 @@ public class StudentControllers {
         this.students.removeIf(student -> student.getId() == id);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable int id, @RequestBody Student studentCurrent){
+        this.students.removeIf(student -> student.getId() == id);
+        this.students.add(studentCurrent);
+        return new ResponseEntity<>(studentCurrent,HttpStatus.OK);
     }
 
 }
