aa1f50cfa80acf9889885cd445c3e5c6b6fcc776
Vinicius Rhuan Oliveira Reis <60737732+ViniciusROReis07@users.noreply.github.com>
2022-02-04 23:28:09 -0300

    feat: Delete student

--- src/main/java/br/com/devdojo/controllers/StudentControllers.java
+++ src/main/java/br/com/devdojo/controllers/StudentControllers.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,17 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.devdojo.model.Student;
 import br.com.devdojo.error.CumstomErrorType;
-import br.com.devdojo.util.DateUtil;
+// import br.com.devdojo.util.DateUtil;
 
-import java.time.LocalDateTime;
+// import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 @RestController
 @RequestMapping("students")
 public class StudentControllers {
 
-    @Autowired
-    private DateUtil dateUtil;
+    // @Autowired
+    // private DateUtil dateUtil;
     private List<Student> students = new ArrayList<>();
 
     @GetMapping("/list")
@@ -46,13 +47,21 @@ public class StudentControllers {
             return new ResponseEntity<>(studentFind, HttpStatus.OK);
         }
 
-        return new ResponseEntity<>(new CumstomErrorType("Student not fount"),HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new CumstomErrorType("Student not fount"), HttpStatus.NOT_FOUND);
     }
 
-
     @PostMapping("/")
-    public ResponseEntity<?> cadastrar(@RequestBody Student student) {
+    public ResponseEntity<?> register(@RequestBody Student student) {
         this.students.add(student);
         return new ResponseEntity<>(student, HttpStatus.OK);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable int id) {
+
+        this.students.removeIf(student -> student.getId() == id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
